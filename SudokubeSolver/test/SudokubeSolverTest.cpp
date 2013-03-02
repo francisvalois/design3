b69cbba93c2a9ecb5aa3338455d5014818738077
osylvain <g.oliviersylvain@gmail.com>
2013-03-02 09:24:16 -0500

    Petite modif d'un test

--- SudokubeSolver/test/SudokubeSolverTest.cpp
+++ SudokubeSolver/test/SudokubeSolverTest.cpp
@@ -21,14 +21,14 @@ namespace {
   TEST_F(SudokubeSolverTest, print) {
 
 	  sudokube.setCaseValue(1,1,2,6);
-//	  sudokube.setCaseValue(1,1,3,2);
-//	  sudokube.setCaseValue(1,1,4,8);
+	  sudokube.setCaseValue(1,1,3,2);
+	  sudokube.setCaseValue(1,1,4,8);
 	  sudokube.setCaseValue(1,4,1,4);
-//	  sudokube.setCaseValue(2,1,2,5);
+	  sudokube.setCaseValue(2,1,2,5);
 	  sudokube.setCaseValue(2,2,3,2);
-//	  sudokube.setCaseValue(2,3,2,8);
+	  sudokube.setCaseValue(2,3,2,8);
 	  sudokube.setCaseValue(3,1,1,1);
-//	  sudokube.setCaseValue(3,2,4,4);
+	  sudokube.setCaseValue(3,2,4,4);
 	  sudokube.setCaseValue(3,4,3,3);
 
 	  sudokube.print();
