dc7ecff628ddd168344fff0ea90ba9a948e0c92c
phbou72 <philippe.bourdages@gmail.com>
2013-03-07 22:42:14 -0500

    Extraction des chiffres et insert dans le sudokube.

--- app/src/kinocto/src/SudocubeExtractor.cpp
+++ app/src/kinocto/src/SudocubeExtractor.cpp
@@ -15,15 +15,8 @@ SudocubeExtractor::~SudocubeExtractor() {
 }
 
 Sudokube SudocubeExtractor::extractSudocube(Mat & src) {
-    //TODO À faire, mettre le contenu extrait de la méthode extractNumbers
-    extractNumbers(src);
-
-    Sudokube sudokube;
-    return sudokube;
-}
-
-void SudocubeExtractor::extractNumbers(Mat & src) {
     cout << "sudocube no " << sudocubeNo << endl;
+    Sudokube sudokube;
     Mat srcGray;
     cleanGraySrc(src, srcGray);
 
@@ -33,7 +26,7 @@ void SudocubeExtractor::extractNumbers(Mat & src) {
     Rect frameRect = getFrameRect(srcHSV);
     if (frameRect.area() == 0) { //TODO Gestion exception
         cout << "Could not find the green frame" << endl;
-        return;
+        return sudokube;
     }
 
     Mat frameCroppedGray = srcGray(frameRect);
@@ -44,26 +37,28 @@ void SudocubeExtractor::extractNumbers(Mat & src) {
 
     if (squaresAreExtracted == false) {
         cout << "Could not extract all the square for sudocube" << endl; //TODO gestion d'exception
-        return;
+        return sudokube;
     }
 
     SquarePair redSquarePair = getRedSquarePair(srcHSV(frameRect));
-    cout << squaresPair.size() << endl;
     squaresPair.push_back(redSquarePair);
-    cout << squaresPair.size() << endl;
-    //squaresPair[squaresPair.size() - 1] = redSquarePair;
 
     vector<vector<SquarePair> > orderedSquaresPair = sortSquaresPair(squaresPair, frameCroppedThresholded.cols);
-    vector<vector<int> > orderedNumber;
 
+    vector<vector<int> > orderedNumber(8);
     for (int i = 0; i < 8; i++) {
-        vector<SquarePair>::iterator it;
-        cout << "nb of row" << orderedSquaresPair[i].size() << endl;
-        for (it = orderedSquaresPair[i].begin(); it != orderedSquaresPair[i].end(); it++) {
-            int y = distance(orderedSquaresPair[i].begin(), it);
+        orderedNumber[i] = vector<int>(8);
+        vector<int>::iterator itNum = orderedNumber[i].begin();
+
+        vector<SquarePair>::iterator itPair;
+        for (itPair = orderedSquaresPair[i].begin(); itPair != orderedSquaresPair[i].end(); itPair++, itNum++) {
+            if (itPair->rect.x == redSquarePair.rect.x && itPair->rect.y == redSquarePair.rect.y) {
+                (*itNum) = -1;
+                continue;
+            }
 
             Mat squareMask = Mat::zeros(frameCroppedThresholded.size(), CV_8UC1);
-            fillConvexPoly(squareMask, it->poly, white);
+            fillConvexPoly(squareMask, itPair->poly, white);
 
             Mat squareMasked = Mat::ones(frameCroppedThresholded.size(), CV_8UC3);
             frameCroppedThresholded.copyTo(squareMasked, squareMask);
@@ -76,7 +71,10 @@ void SudocubeExtractor::extractNumbers(Mat & src) {
 
             if (foundPossibleNumber == true) {
                 int numberFound = numberReader.identifyNumber(number);
+                (*itNum) = numberFound;
+
                 //cout << "Found : " << numberFound << endl;
+                int y = distance(orderedSquaresPair[i].begin(), itPair);
                 sprintf(filename, "%s/number/%d_%d_%d.png", OUTPUT_PATH, sudocubeNo, i + 1, y);
                 saveImage(number, filename);
             }
@@ -84,7 +82,11 @@ void SudocubeExtractor::extractNumbers(Mat & src) {
     }
 
     sudocubeNo++;
+    insertAllNumber(sudokube, orderedNumber);
+
+    return sudokube;
 }
+
 
 void SudocubeExtractor::cleanGraySrc(Mat& src, Mat& srcGray) {
     cvtColor(src, srcGray, CV_BGR2GRAY);
@@ -101,7 +103,7 @@ Rect SudocubeExtractor::getFrameRect(Mat& srcHSV) {
     applyErode(segmentedFrame, FRAME_ERODE_SIZE, MORPH_ELLIPSE);
     applyDilate(segmentedFrame, FRAME_DILATE_SIZE, MORPH_RECT);
     sprintf(filename, "%s/frameSeg/%d.png", OUTPUT_PATH, sudocubeNo);
-    saveImage(segmentedFrame, filename);
+    //saveImage(segmentedFrame, filename);
 
     vector<vector<Point> > frameContours;
     vector<Vec4i> frameHierarchy;
@@ -145,7 +147,7 @@ bool SudocubeExtractor::findSquaresPair(const Mat& srcGray, vector<SquarePair> &
     for (int threshValue = SQUARE_THRESHOLD_MIN; threshValue <= SQUARE_THRESHOLD_MAX && isExtracted == false; threshValue++) {
         threshold(srcGray, srcThresholded, threshValue, 500, THRESH_BINARY);
         sprintf(filename, "%s/sudocubeThresh/%d.png", OUTPUT_PATH, sudocubeNo);
-        saveImage(srcThresholded, filename);
+        //saveImage(srcThresholded, filename);
 
         vector<vector<Point> > squaresContours;
         vector<Vec4i> squaresHierarchy;
@@ -282,6 +284,68 @@ bool SudocubeExtractor::extractNumber(Mat &inImage, Mat &outImage, Mat &squareMa
     return false;
 }
 
+void SudocubeExtractor::insertAllNumber(Sudokube & sudokube, vector<vector<int> > orderedNumber) {
+    insert(sudokube, 1, 1, 1, orderedNumber[3][7]);
+    insert(sudokube, 1, 1, 2, orderedNumber[2][6]);
+    insert(sudokube, 1, 1, 3, orderedNumber[1][5]);
+    insert(sudokube, 1, 1, 4, orderedNumber[0][4]);
+    insert(sudokube, 1, 2, 1, orderedNumber[3][6]);
+    insert(sudokube, 1, 2, 2, orderedNumber[2][5]);
+    insert(sudokube, 1, 2, 3, orderedNumber[1][4]);
+    insert(sudokube, 1, 2, 4, orderedNumber[0][3]);
+    insert(sudokube, 1, 3, 1, orderedNumber[3][5]);
+    insert(sudokube, 1, 3, 2, orderedNumber[2][4]);
+    insert(sudokube, 1, 3, 3, orderedNumber[1][3]);
+    insert(sudokube, 1, 3, 4, orderedNumber[0][2]);
+    insert(sudokube, 1, 4, 1, orderedNumber[3][4]);
+    insert(sudokube, 1, 4, 2, orderedNumber[2][3]);
+    insert(sudokube, 1, 4, 3, orderedNumber[1][2]);
+    insert(sudokube, 1, 4, 4, orderedNumber[0][1]);
+
+    insert(sudokube, 2, 1, 1, orderedNumber[4][6]);
+    insert(sudokube, 2, 1, 2, orderedNumber[4][5]);
+    insert(sudokube, 2, 1, 3, orderedNumber[4][4]);
+    insert(sudokube, 2, 1, 4, orderedNumber[4][3]);
+    insert(sudokube, 2, 2, 1, orderedNumber[5][5]);
+    insert(sudokube, 2, 2, 2, orderedNumber[5][4]);
+    insert(sudokube, 2, 2, 3, orderedNumber[5][3]);
+    insert(sudokube, 2, 2, 4, orderedNumber[5][2]);
+    insert(sudokube, 2, 3, 1, orderedNumber[6][4]);
+    insert(sudokube, 2, 3, 2, orderedNumber[6][3]);
+    insert(sudokube, 2, 3, 3, orderedNumber[6][2]);
+    insert(sudokube, 2, 3, 4, orderedNumber[6][1]);
+    insert(sudokube, 2, 4, 1, orderedNumber[7][3]);
+    insert(sudokube, 2, 4, 2, orderedNumber[7][2]);
+    insert(sudokube, 2, 4, 3, orderedNumber[7][1]);
+    insert(sudokube, 2, 4, 4, orderedNumber[7][0]);
+
+    insert(sudokube, 3, 1, 1, orderedNumber[0][0]);
+    insert(sudokube, 3, 1, 2, orderedNumber[1][0]);
+    insert(sudokube, 3, 1, 3, orderedNumber[2][0]);
+    insert(sudokube, 3, 1, 4, orderedNumber[3][0]);
+    insert(sudokube, 3, 2, 1, orderedNumber[1][1]);
+    insert(sudokube, 3, 2, 2, orderedNumber[2][1]);
+    insert(sudokube, 3, 2, 3, orderedNumber[3][1]);
+    insert(sudokube, 3, 2, 4, orderedNumber[4][0]);
+    insert(sudokube, 3, 3, 1, orderedNumber[2][2]);
+    insert(sudokube, 3, 3, 2, orderedNumber[3][2]);
+    insert(sudokube, 3, 3, 3, orderedNumber[4][1]);
+    insert(sudokube, 3, 3, 4, orderedNumber[5][0]);
+    insert(sudokube, 3, 4, 1, orderedNumber[3][3]);
+    insert(sudokube, 3, 4, 2, orderedNumber[4][2]);
+    insert(sudokube, 3, 4, 3, orderedNumber[5][1]);
+    insert(sudokube, 3, 4, 4, orderedNumber[6][0]);
+}
+
+void SudocubeExtractor::insert(Sudokube & sudokube, int face, int j, int k, int value) {
+    if (value > 0) {
+        sudokube.setCaseValue(face, j, k, value);
+    } else if (value == -1) {
+        sudokube.setRedCase(face, j, k);
+    }
+}
+
+
 void SudocubeExtractor::applyErode(Mat & toErode, int size, int morphShape) {
     Point erodePoint(size, size);
     Mat erodeElem = getStructuringElement(morphShape, Size(2 * size + 1, 2 * size + 1), erodePoint);
@@ -301,3 +365,5 @@ void SudocubeExtractor::saveImage(Mat &pict, char* filename) {
 
     imwrite(filename, pict, compression_params);
 }
+
+
