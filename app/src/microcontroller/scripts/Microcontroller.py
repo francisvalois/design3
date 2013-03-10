15a5af9b450f52c2b3983531f24dc69b8720a258
phbou72 <philippe.bourdages@gmail.com>
2013-03-10 15:30:12 -0400

    Ajout de commande pour demander les paramètres de l'antenne et l'exécution de déplacements.

--- app/src/microcontroller/scripts/Microcontroller.py
+++ app/src/microcontroller/scripts/Microcontroller.py
@@ -11,11 +11,13 @@ from microcontroller.srv import *
 def handlePutPen(req):
     print "put pen down : \r"
     print req.down
+    
     return PutPenResponse()
 
 def handleWriteToLCD(req):
     print "Write to LCD messsage : \r"
     print req.message
+    
     return WriteToLCDResponse()
 
 def handleDrawNumber(req):
@@ -23,12 +25,32 @@ def handleDrawNumber(req):
     print req.number
     print "Draw it big? \r"
     print req.isBig
+    
     return DrawNumberResponse()
 
 def handleTurnLED(req):
     print "Turn LED on \r"
     print req.on
+    
     return TurnLEDResponse()
+
+def handleExecuteMove(req):
+    print "Executing move with angle \r"
+    print req.angle
+    print "and distance of \r"
+    print req.distance
+    
+    return ExecuteMoveResponse()
+
+def handleDecodeAntenna(req):
+    print "Decoding antenna \r"
+    
+    request = DecodeAntennaResponse()
+    request.isBig = False
+    request.orientation = DecodeAntennaResponse.SOUTH
+    request.number = 8
+    
+    return request
 
 def Microcontroller():
     rospy.init_node('microcontroller')
@@ -37,6 +59,8 @@ def Microcontroller():
     s = rospy.Service('microcontroller/writeToLCD', WriteToLCD, handleWriteToLCD)
     s = rospy.Service('microcontroller/drawNumber', DrawNumber, handleDrawNumber)
     s = rospy.Service('microcontroller/turnLED', TurnLED, handleTurnLED)
+    s = rospy.Service('microcontroller/executeMove', ExecuteMove, handleExecuteMove)
+    s = rospy.Service('microcontroller/decodeAntenna', DecodeAntenna, handleDecodeAntenna)
 
     rospy.spin()
 
