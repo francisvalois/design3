9b8e208103782cc3b30e4e9b94f83472ac75f328
osylvain <g.oliviersylvain@gmail.com>
2013-03-15 16:10:08 -0400

    Terminé les tests d'intégration sur pathplanning

--- app/src/kinocto/test/PathPlanningTest.cpp
+++ app/src/kinocto/test/PathPlanningTest.cpp
@@ -1,16 +1,14 @@
+//**************************************************************************************
+//FOR NOW, ALL THESE TESTS REQUIRE VISUAL VERIFICATION TO BE PASSED
+//TO DO SO. UNCOMMENT LINES WITH pathPlanning.printTable(); AND VERIFY MANUALLY
+//**************************************************************************************
+
 #include <gtest/gtest.h>
 
 #include "PathPlanning2.h"
 
 namespace {
-	Position SUDOKUBE1 = Position(176,78);
-	Position SUDOKUBE2 = Position(209,78);
-	Position SUDOKUBE3 = Position(197,96);
-	Position SUDOKUBE4 = Position(197,70);
-	Position SUDOKUBE5 = Position(197,44);
-	Position SUDOKUBE6 = Position(197,18);
-	Position SUDOKUBE7 = Position(209,36);
-	Position SUDOKUBE8 = Position(176,36);
+
 
   class PathPlanningTest : public ::testing::Test {
   protected:
@@ -43,12 +41,11 @@ namespace {
     	SUDOKUBE7.y = 36;
     	SUDOKUBE8.x = 176;
     	SUDOKUBE8.y = 36;
+
+		start.x = 36;
+		start.y = 85;
     }
-
   };
-
-  //FOR NOW, ALL THESE TESTS REQUIRE VISUAL VERIFICATION TO BE PASSED
-  //TO DO SO. UNCOMMENT LINES WITH pathPlanning.printTable(); AND VERIFY MANUALLY
 
   TEST_F(PathPlanningTest, IntegrationTest1) {
 	  	//CONFIGURATION POSSIBILITY 1
@@ -67,8 +64,7 @@ namespace {
 		obs1.y = 85;
 		obs2.x = 120;
 		obs2.y = 25;
-		start.x = 36;
-		start.y = 85;
+
 		pathPlanning.setObstacles(obs1,obs2);
 
 		pathPlanning.getPath(start, SUDOKUBE1);
@@ -95,4 +91,280 @@ namespace {
 		pathPlanning.getPath(start, SUDOKUBE8);
 //		pathPlanning.printTable();
   }
+
+  TEST_F(PathPlanningTest, IntegrationTest2) {
+	  	//CONFIGURATION POSSIBILITY 2
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+	  	obs1.x = 90;
+		obs1.y = 85;
+		obs2.x = 110;
+		obs2.y = 85;
+
+		pathPlanning.setObstacles(obs1,obs2);
+
+		pathPlanning.getPath(start, SUDOKUBE1);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE2);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE3);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE4);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE5);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE6);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE7);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE8);
+//		pathPlanning.printTable();
+  }
+
+  TEST_F(PathPlanningTest, IntegrationTest3) {
+	  	//CONFIGURATION POSSIBILITY 3
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx0000000000xxxxxxxxxxx00000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+	  	obs1.x = 90;
+		obs1.y = 65;
+		obs2.x = 110;
+		obs2.y = 65;
+
+		pathPlanning.setObstacles(obs1,obs2);
+
+		pathPlanning.getPath(start, SUDOKUBE1);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE2);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE3);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE4);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE5);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE6);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE7);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE8);
+//		pathPlanning.printTable();
+  }
+
+  TEST_F(PathPlanningTest, IntegrationTest4) {
+//	  	CONFIGURATION POSSIBILITY 4
+//	  	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//	  	  xx00000000000000000000000000000000000xx
+//	  	  xx00000000000000000000000000000000000xx
+//	  	  xx0000000000xxxxx00000xxxxx0000000000xx
+//	  	  xx0000000000xxxxx00000xxxxx0000000000xx
+//	  	  xx0000000000xxxxx00000xxxxx0000000000xx
+//	  	  xx00000000000000000000000000000000000xx
+//	  	  xx00000000000000000000000000000000000xx
+//	  	  xx00000000000000000000000000000000000xx
+//	  	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+	  	obs1.x = 90;
+		obs1.y = 55;
+		obs2.x = 150;
+		obs2.y = 55;
+
+		pathPlanning.setObstacles(obs1,obs2);
+
+		pathPlanning.getPath(start, SUDOKUBE1);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE2);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE3);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE4);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE5);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE6);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE7);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE8);
+//		pathPlanning.printTable();
+  }
+
+  TEST_F(PathPlanningTest, IntegrationTest5) {
+	  	//CONFIGURATION POSSIBILITY 5
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//	  xx00000000000000000000000000000000000xx
+//	  xx0000000000xxxxxx0000000000000000000xx
+//	  xx0000000000xxxxxx0000000000000000000xx
+//	  xx0000000000xxxxxx0000000000000000000xx
+//	  xx000000000000000000xxxxxx00000000000xx
+//	  xx000000000000000000xxxxxx00000000000xx
+//	  xx000000000000000000xxxxxx00000000000xx
+//	  xx00000000000000000000000000000000000xx
+//	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+	  	obs1.x = 90;
+		obs1.y = 65;
+		obs2.x = 150;
+		obs2.y = 45;
+
+		pathPlanning.setObstacles(obs1,obs2);
+
+		pathPlanning.getPath(start, SUDOKUBE1);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE2);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE3);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE4);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE5);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE6);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE7);
+//		pathPlanning.printTable();
+
+		pathPlanning.getPath(start, SUDOKUBE8);
+//		pathPlanning.printTable();
+  }
+
+ TEST_F(PathPlanningTest, IntegrationTest6) {
+  	  	//CONFIGURATION POSSIBILITY 5
+  //	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  //	  xx00000000000000000000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx00000000000000000000000000000000000xx
+  //	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  	  	obs1.x = 90;
+  		obs1.y = 65;
+  		obs2.x = 150;
+  		obs2.y = 45;
+
+		start.x = 36;
+		start.y = 55;
+
+  		pathPlanning.setObstacles(obs1,obs2);
+
+  		pathPlanning.getPath(start, SUDOKUBE1);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE2);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE3);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE4);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE5);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE6);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE7);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE8);
+//  		pathPlanning.printTable();
+    }
+
+ TEST_F(PathPlanningTest, IntegrationTest7) {
+  	  	//CONFIGURATION POSSIBILITY 5
+  //	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  //	  xx00000000000000000000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx0000000000xxxxxx0000000000000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx000000000000000000xxxxxx00000000000xx
+  //	  xx00000000000000000000000000000000000xx
+  //	  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+  	  	obs1.x = 90;
+  		obs1.y = 65;
+  		obs2.x = 150;
+  		obs2.y = 45;
+
+		start.x = 40;
+		start.y = 25;
+
+  		pathPlanning.setObstacles(obs1,obs2);
+
+  		pathPlanning.getPath(start, SUDOKUBE1);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE2);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE3);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE4);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE5);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE6);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE7);
+//  		pathPlanning.printTable();
+
+  		pathPlanning.getPath(start, SUDOKUBE8);
+//  		pathPlanning.printTable();
+    }
 }
