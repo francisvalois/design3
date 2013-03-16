e782e20d059b518d08a8bcb48468f043c940343a
phbou72 <philippe.bourdages@gmail.com>
2013-03-16 15:43:11 -0400

    MAJ du code pour extraire des images de la kinect...

--- app/src/basestation/src/KinectCapture.cpp
+++ app/src/basestation/src/KinectCapture.cpp
@@ -10,6 +10,9 @@ KinectCapture::~KinectCapture() {
 
 Mat KinectCapture::captureDepthMatrix() {
     Mat world;
+    Mat showRGB;
+    Mat depthMap;
+    Mat show;
 
     VideoCapture capture;
     capture.open(CV_CAP_OPENNI);
@@ -17,7 +20,10 @@ Mat KinectCapture::captureDepthMatrix() {
 
     if (capture.isOpened() == true) {
         capture.grab();
-        capture.retrieve(world, CV_CAP_OPENNI_POINT_CLOUD_MAP);
+        //capture.retrieve(world, CV_CAP_OPENNI_POINT_CLOUD_MAP);
+        capture.retrieve(showRGB, CV_CAP_OPENNI_BGR_IMAGE);
+        if (capture.retrieve(depthMap, CV_CAP_OPENNI_DEPTH_MAP))
+            depthMap.convertTo(show, CV_8UC1, 0.05f);
     } else {
         ROS_ERROR("COULD NOT CAPTURE A PICTURE WITH THE KINECT");
     }
