a47b94f48257a3fb66b3aee2d1916c21e6e2e68d
Pierre-Luc Buhler <plbuhler01@gmail.com>
2013-03-29 19:03:28 -0400

    Recorretion d'une correction enlevé par erreur

--- app/src/microcontroller/scripts/Microcontroller.py
+++ app/src/microcontroller/scripts/Microcontroller.py
@@ -219,6 +219,7 @@ def sendCommandToController(commande):
     
     rospy.loginfo("Sending command to microcontroller %s", commande)
     
+    response = ""
     try: 
         ser.open()
         
@@ -226,7 +227,7 @@ def sendCommandToController(commande):
             ser.write(bytes(commande))
             time.sleep(0.5)  # le temps que le microcontrolleur recoive la commande
 
-            response = None
+
             while(response != 'E'):
                 response = ser.readline()  # Boucle while ici?? 
             print(repr("read data:" + response))
