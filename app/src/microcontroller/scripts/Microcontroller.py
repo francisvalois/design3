3be3179e7a273cd2f7dc3cdcda4dffaa115fcc1c
Pierre-Luc Buhler <plbuhler01@gmail.com>
2013-03-29 19:01:08 -0400

    Microcontroller.py : petites corrections

--- app/src/microcontroller/scripts/Microcontroller.py
+++ app/src/microcontroller/scripts/Microcontroller.py
@@ -156,7 +156,7 @@ def handleRotate(req):
     return RotateResponse()
 
 def handleDecodeAntenna(req):
-
+    response = DecodeAntennaResponse()
     fini = False
     index = 0
     while fini == False:
@@ -192,7 +192,7 @@ def handleDecodeAntenna(req):
                     response.isBig = True
                 else:
                     response.isBig = False
-                break;
+                fini = True
             else:
                 print("!!!! PAS DE SUCCES !!!!")
                 
@@ -218,7 +218,7 @@ def sendCommandToController(commande):
     global ser
     
     rospy.loginfo("Sending command to microcontroller %s", commande)
-    response = ""
+    
     try: 
         ser.open()
         
@@ -226,6 +226,7 @@ def sendCommandToController(commande):
             ser.write(bytes(commande))
             time.sleep(0.5)  # le temps que le microcontrolleur recoive la commande
 
+            response = None
             while(response != 'E'):
                 response = ser.readline()  # Boucle while ici?? 
             print(repr("read data:" + response))
