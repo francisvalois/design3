63be28a3d38865b17f2d0254bc5b8fb1c8d35d60
phbou72 <philippe.bourdages@gmail.com>
2013-04-01 18:54:11 -0400

    Correction d'un bug qui empêchais la station de base de se compiler

--- app/src/basestation/src/BaseStation.cpp
+++ app/src/basestation/src/BaseStation.cpp
@@ -129,16 +129,15 @@ bool BaseStation::findRobotPosition(FindRobotPosition::Request & request, FindRo
     }
 
     robotDetection.findRobotWithAngle(depthMatrix, rgbMatrix);
-    //Vec2f robot = robotDetection.getRobotPosition(); //TEST TEMPORAIRE
+    Vec2f robot = robotDetection.getRobotPosition(); //TEST TEMPORAIRE
 
-    response.x = 36.5;
+    response.x = 36.5; // TODO À supprimer quand la kinect fonctionnera
     response.y = 79.0;
 
     ROS_INFO( "%s x:%f y:%f", "Request Find Robot Position. Sending Values ", 36.5, 79.0);
 
     stringstream info;
-    info << "Kinocto : Demande de la position du robot \n";
-    info << "Envoi de la position : ";
+    info << "Kinocto : Demande de la position du robot \n Envoi de la position : ";
     info << " (" << robot[1] << "," << robot[0] << ")";
     QString infoQ((char*) info.str().c_str());
 
