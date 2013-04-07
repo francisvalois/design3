8ccea546f84b1a08bf1b348d774fe93da8246427
phbou72 <philippe.bourdages@gmail.com>
2013-04-07 06:03:58 -0400

    MAJ des images pour les tests

--- app/src/kinocto/test/vision/FrameCenterFinderIT.cpp
+++ app/src/kinocto/test/vision/FrameCenterFinderIT.cpp
@@ -15,44 +15,44 @@ protected:
 };
 
 TEST_F(FrameCenterFinderIT, findCenterFrame1) {
-    Mat img = imread("img/testFrameCenterFinder/1.png");
-    double translateX = frameCenterFinder.getXTranslation(img);
-
-    ASSERT_NEAR(-2.2, translateX, PRECISION);
-}
-
-TEST_F(FrameCenterFinderIT, findCenterFrame2) {
-    Mat img = imread("img/testFrameCenterFinder/2.png");
-    double translateX = frameCenterFinder.getXTranslation(img);
-
-    ASSERT_NEAR(2.59, translateX, PRECISION);
-}
-
-TEST_F(FrameCenterFinderIT, findCenterFrame3) {
-    Mat img = imread("img/testFrameCenterFinder/3.png");
-    double translateX = frameCenterFinder.getXTranslation(img);
-
-    ASSERT_NEAR(6.1, translateX, PRECISION);
-}
-
-TEST_F(FrameCenterFinderIT, findCenterFrame4) {
-    Mat img = imread("img/testFrameCenterFinder/4.png");
-    double translateX = frameCenterFinder.getXTranslation(img);
-
-    ASSERT_NEAR(-3.67, translateX, PRECISION);
-}
-
-TEST_F(FrameCenterFinderIT, findCenterFrame5) {
-    Mat img = imread("img/testFrameCenterFinder/5.png");
+    Mat img = imread("img/testFrameCenterFinder/1.jpg");
     double translateX = frameCenterFinder.getXTranslation(img);
 
     ASSERT_NEAR(2.2, translateX, PRECISION);
 }
 
-TEST_F(FrameCenterFinderIT, findCenterFrame6) {
-    Mat img = imread("img/testFrameCenterFinder/6.png");
+TEST_F(FrameCenterFinderIT, findCenterFrame2) {
+    Mat img = imread("img/testFrameCenterFinder/2.jpg");
     double translateX = frameCenterFinder.getXTranslation(img);
 
-    ASSERT_NEAR(-1.5, translateX, PRECISION);
+    ASSERT_NEAR(-2.55, translateX, PRECISION);
+}
+
+TEST_F(FrameCenterFinderIT, findCenterFrame3) {
+    Mat img = imread("img/testFrameCenterFinder/3.jpg");
+    double translateX = frameCenterFinder.getXTranslation(img);
+
+    ASSERT_NEAR(-6.85, translateX, PRECISION);
+}
+
+TEST_F(FrameCenterFinderIT, findCenterFrame4) {
+    Mat img = imread("img/testFrameCenterFinder/4.jpg");
+    double translateX = frameCenterFinder.getXTranslation(img);
+
+    ASSERT_NEAR(0.75, translateX, PRECISION);
+}
+
+TEST_F(FrameCenterFinderIT, findCenterFrame5) {
+    Mat img = imread("img/testFrameCenterFinder/5.jpg");
+    double translateX = frameCenterFinder.getXTranslation(img);
+
+    ASSERT_NEAR(-5.6, translateX, PRECISION);
+}
+
+TEST_F(FrameCenterFinderIT, findCenterFrame6) {
+    Mat img = imread("img/testFrameCenterFinder/6.jpg");
+    double translateX = frameCenterFinder.getXTranslation(img);
+
+    ASSERT_NEAR(1.75, translateX, PRECISION);
 }
 }
