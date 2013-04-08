42c580f5cee5b5df67b90e1a57fe901c5f637fe5
Francis Valois <francis.valois1@gmail.com>
2013-04-07 20:49:26 -0400

    Correction des tests en double

--- app/src/basestation/test/AllTests.cpp
+++ app/src/basestation/test/AllTests.cpp
@@ -1,11 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "KinectCalibratorTest.cpp"
-#include "KinectTransformatorTest.cpp"
-#include "ObjectDetectorTest.cpp"
-#include "ObstacleDetectorTest.cpp"
-#include "RobotDetectorTest.cpp"
-
 
 
 int main(int argc, char **argv) {
