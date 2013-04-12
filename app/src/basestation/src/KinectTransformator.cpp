9e2242fb620bb1b9c268525c2846d68ef79928b9
Francis Valois <uberpwn315@gmail.com>
2013-04-12 19:41:13 -0400

    LookupTable Addition

--- app/src/basestation/src/KinectTransformator.cpp
+++ app/src/basestation/src/KinectTransformator.cpp
@@ -60,31 +60,76 @@ Vec2f KinectTransformator::getTrueCoordFromKinectCoord(Vec3f depthXYZ) {
 
 
 
-Point KinectTransformator::findNeareastValueInLookupTable(int expectedValueX, int expectedValueY){
-    float B22data[] = {1, 2, 3, 4};
-    Mat B22 = Mat(2, 2, CV_32F, B22data).clone();
-    cout << B22 << endl;
+Point KinectTransformator::findNeareastValueInLookupTable(float expectedValueX, float expectedValueY){
+    expectedValueY = 1.5;
+    expectedValueX = 1.3;
 
-    vector<float> test3 = B22.row(0);
+    float B22data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    Mat lookupTable = Mat(4, 4, CV_32F, B22data).clone();
 
-    cout << test3[0] << endl;
+    vector<float> yRow = lookupTable.row(0);
+    Mat lookupTableTransposed = lookupTable.t();
+    vector<float> xRow = lookupTableTransposed.row(0);
 
-    struct {
-        int operator()(int a, int b) {return a > b;}
-    } comparator;
+    cout << xRow[0] << " " << xRow[1] << endl;
 
-    //std::vector<int>::iterator it = std::find_if (test3.begin(), test3.end(), comparator);
-    //std::cout << "The first odd value is " << *it << '\n';
+    //Find X and Y Position of each points near the expected values
+    std::vector<float>::iterator lowBoundY, lowBoundX;
+    lowBoundY = std::lower_bound (yRow.begin(), yRow.end(), expectedValueY);
+    int lowBoundYIndex = distance(yRow.begin(), lowBoundY) - 1;
 
-    std::vector<float>::iterator low;
-    low=std::lower_bound (test3.begin(), test3.end(), expectedValueY);
-
-    float beforeValue = low - test3.begin();
-
-
+    lowBoundX = std::lower_bound (xRow.begin(), xRow.end(), expectedValueX);
+    int lowBoundXIndex = distance(xRow.begin(), lowBoundX) - 1;
 
 
+    //Find the values for the quad interpolate
+    Vec3f interpolate1, interpolate2, interpolate3, interpolate4;
+
+    float point11 = lookupTable.at<float>(lowBoundXIndex , lowBoundYIndex);
+    float point12 = lookupTable.at<float>(lowBoundXIndex , lowBoundYIndex + 1);
+    float point21 = lookupTable.at<float>(lowBoundXIndex + 1, lowBoundYIndex);
+    float point22 = lookupTable.at<float>(lowBoundXIndex + 1, lowBoundYIndex + 1);
+
+    if(lookupTable.rows >= lowBoundYIndex + 2){
+        float point31 = lookupTable.at<float>(lowBoundXIndex + 2, lowBoundYIndex);
+        float point32 = lookupTable.at<float>(lowBoundXIndex + 2, lowBoundYIndex + 1);
+        interpolate3 = Vec3f(point11, point21, point31);
+        interpolate4 = Vec3f(point12, point22, point32);
+    }else{
+        float point31 = lookupTable.at<float>(lowBoundXIndex - 1, lowBoundYIndex);
+        float point32 = lookupTable.at<float>(lowBoundXIndex - 1, lowBoundYIndex + 1);
+        interpolate3 = Vec3f(point31, point11, point21);
+        interpolate4 = Vec3f(point32, point22, point32);
+    }
+
+    if(lookupTable.cols >= lowBoundXIndex + 2){
+        float point13 = lookupTable.at<float>(lowBoundXIndex, lowBoundYIndex + 2);
+        float point23 = lookupTable.at<float>(lowBoundXIndex + 1, lowBoundYIndex + 2);
+        interpolate1 = Vec3f(point11, point12, point13);
+        interpolate2 = Vec3f(point21, point22, point23);
+    }else{
+        float point13 = lookupTable.at<float>(lowBoundXIndex, lowBoundYIndex - 1);
+        float point23 = lookupTable.at<float>(lowBoundXIndex + 1, lowBoundYIndex - 1);
+        interpolate1 = Vec3f(point13, point11, point12);
+        interpolate2 = Vec3f(point23, point21, point22);
+    }
+
+    float interpolateValue1 = quadInterpolate(expectedValueX, interpolate1[0], interpolate1[1], interpolate1[2]);
+    float interpolateValue2 = quadInterpolate(expectedValueX, interpolate2[0], interpolate2[1], interpolate2[2]);
+    float interpolateValue3 = quadInterpolate(expectedValueY, interpolate3[0], interpolate3[1], interpolate3[2]);
+    float interpolateValue4 = quadInterpolate(expectedValueY, interpolate4[0], interpolate4[1], interpolate4[2]);
 
 
-    cout << low - test3.begin() << endl;
+    cout << point11 << endl;
 }
+
+
+float KinectTransformator::quadInterpolate( float expectedValue, float negPoint, float zeroPoint, float posPoint)
+{
+    //if( expectedValue <= -1 )  return negPoint;
+    //if( expectedValue >= 1 )  return posPoint;
+    float left = zeroPoint - expectedValue * (negPoint - zeroPoint);
+    float right = zeroPoint + expectedValue * (posPoint - zeroPoint);
+    return (left + right + expectedValue * (right - left)) / 2;
+}
+
