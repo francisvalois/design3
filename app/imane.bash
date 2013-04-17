be6ec4c806a52636da09d3a2a785a68f56d05127
phbou72 <philippe.bourdages@gmail.com>
2013-04-16 21:46:03 -0400

    FIX script bash

--- app/imane.bash
+++ app/imane.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-pid=$(ps | grep Kinocto | cut -d ' ' -f2)
+pid=$(ps | grep Kinocto | cut -d ' ' -f1)
+echo ps
 for line in $pid
 do
+   echo $line
    kill -9 $line
 done
 
-pid=$(ps | grep BaseStation | cut -d ' ' -f2)
+pid=$(ps | grep BaseStation | cut -d ' ' -f1)
 for line in $pid
 do
+   echo $line
    kill -9 $line
 done
 
