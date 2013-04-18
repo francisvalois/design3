410ad56642a8562d2fbbc1520a56730d3061729f
Francis Valois <uberpwn315@gmail.com>
2013-04-18 05:39:21 -0400

    fix

--- app/src/basestation/src/KinectTransformator.cpp
+++ app/src/basestation/src/KinectTransformator.cpp
@@ -173,7 +173,7 @@ cv::Vec2f KinectTransformator::distortionCorrection(Vec2f distanceToCorrect) {
 
 cv::Vec2f KinectTransformator::distortionZfromXPosition(cv::Vec2f positionToCorrect) {
     float error = -0.00002 * pow(positionToCorrect[0] * 100, 5) - 0.000003 * pow(positionToCorrect[0] * 100, 4)
-            + 0.0001 * * pow(positionToCorrect[0] * 100, 3) + 0.0029 * pow(positionToCorrect[0] * 100, 2) +
+            + 0.0001 * pow(positionToCorrect[0] * 100, 3) + 0.0029 * pow(positionToCorrect[0] * 100, 2) +
             0.2314 * positionToCorrect[0] + 0.2642;
 
     if (positionToCorrect[0] < -30) {
